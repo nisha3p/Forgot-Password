0afc6598a19e4a8dd0a948f98935f15f865b1d65
Shivakshi <shivakshisheel.srivastava@gmail.com>
2021-07-03 23:40:01 +0530

    YN

--- Forgot_Password.cpp
+++ Forgot_Password.cpp
@@ -1,10 +1,39 @@
 #include<iostream>
-#include <windows.h>
+#include<windows.h>
 #include<string.h>
 using namespace std;
 int heading_color=4;
 int output_color=6;
 int default_color=7;
+
+int Entry()
+{
+    int id;
+    int i;
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 60);
+    for (i = 0; i < 73; i++)
+    {
+        printf("-");
+    }
+    printf(" Forgot Password ");
+    for (i = 0; i < 73; i++)
+    {
+        printf("-");
+    }
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), output_color);
+
+    string Name, Pass;  /*Temp*/
+    printf("\n\tName (In All Caps):");
+    cin>>Name;
+    printf("\n\tPassword:");
+    cin >> Pass;
+    
+    id=1; /*Temp*/
+
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), default_color);
+    return id;
+}
+
 int Home(int id)
 {
     int i;
@@ -51,6 +80,25 @@ int Add_New_Pass(int id)
     cin >> Pass;   
     /*Trial*/
     cout<<"\n\tS="<<Service<<" U="<<U_Name<<" P="<<Pass;
+    
+    adding:
+    char Ans;
+    cout << "\n\n\t Search for another answer (Y/N): ";
+    cin >> Ans;
+    while (Ans != 'y' || Ans != 'Y' || Ans != 'n' || Ans != 'N')
+    {
+        cout << "\n\tInvalid Answer, Please try again!";
+        cout << "\n\n\t Search for another answer (Y/N): ";
+        cin >> Ans;
+    }
+    if (Ans == 'y' || Ans == 'Y')
+    {
+        goto adding;
+    }
+    else if (Ans == 'n' || Ans == 'N')
+    {
+        Home(id);
+    }
     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), default_color);
     return 0;
 }
@@ -117,8 +165,9 @@ int Ret_Pass(int id)
 int main()
 {
     
-    int id =1; //Temp - Replace with SQL var
+    int id; //Temp - Replace with SQL var
     int choice;
+    id=Entry();
     choice=Home(id);
     if(choice==1)
     {
